642dfebd9a35dfc2b954f5182656d806551fc2a3
Dan4541 <93727378+Dan4541@users.noreply.github.com>
2022-05-27 18:37:12 -0600

    diseño de vistas

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,38 @@
+<div class="col-2 h-100" style="background-color: #045399">
+    
+    <div class="justify-content-center py-2">
+   
+        <div class="row">
+            <div class="col-4 mx-auto py-3">
+                <a href="">
+                    <img src="/img/dupdo.svg" alt="" height="50px" width="50px">
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-4 mx-auto py-2">
+                <label for=""><strong style="color: black">Paginas</strong></label>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="justify-content-center py-2">
+        
+        <div class="row">
+            <div class="col-4 mx-auto py-3">
+                <a href="">
+                    <img src="/img/ajustes.svg" alt="" height="50px" width="50px">
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-7 mx-auto py-2">
+                <label for=""><strong style="color: black">Configuraci&oacute;n</strong></label>
+            </div>
+        </div>        
+    </div>
+
+</div>
---
+++ resources/views/components/upbar.blade.php
@@ -0,0 +1,12 @@
+<div class="row">
+
+    <nav class="navbar bg-dark">
+        <div class="container-fluid py-3">
+          <a class="navbar-brand text-white">{{ "Gestión de Paginas" }}</a>
+          <form class="d-flex" role="search">        
+            <button class="btn btn-outline-light" type="submit">Ver sitio Web</button>
+          </form>
+        </div>
+      </nav>
+
+</div>
